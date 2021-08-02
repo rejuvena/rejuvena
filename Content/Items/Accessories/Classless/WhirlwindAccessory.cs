a854df87b2663a76c155903e8daf5c6d508d477d
Stevie <xxlennygamerxx@gmail.com>
2021-08-01 19:07:56 -0700

    Add a Whirlwind Tome sprite so Terra leaves me alone.

--- Content/Items/Accessories/Classless/WhirlwindAccessory.cs
+++ Content/Items/Accessories/Classless/WhirlwindAccessory.cs
@@ -1,4 +1,5 @@
-﻿using Rejuvena.Assets;
+﻿using Microsoft.Xna.Framework;
+using Rejuvena.Assets;
 using Rejuvena.Content.Players.AccessoryHandlers;
 using Terraria;
 
@@ -10,6 +11,13 @@ namespace Rejuvena.Content.Items.Accessories.Classless
     [FallbackAsset(FallbackAssetType.Tome)]
     public class WhirlwindAccessory : AccessoryItem
     {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            Item.Size = new Vector2(28f, 32f);
+        }
+
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             base.UpdateAccessory(player, hideVisual);
