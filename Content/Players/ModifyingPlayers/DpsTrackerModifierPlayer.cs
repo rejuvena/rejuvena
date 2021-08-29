4b15758e86937bd0a25896731fd332c6059828f1
Stevie <xxlennygamerxx@gmail.com>
2021-08-29 16:15:37 -0700

    Sensible DPS calculations.

---
+++ Content/Players/ModifyingPlayers/DpsTrackerModifierPlayer.cs
@@ -0,0 +1,49 @@
+﻿#region License
+// Copyright (C) 2021 Tomat and Contributors
+// GNU General Public License Version 3, 29 June 2007
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader.IO;
+
+namespace Rejuvena.Content.Players.ModifyingPlayers
+{
+    public class DpsTrackerModifierPlayer : RejuvenaPlayer
+    {
+        public List<(int, uint)> DamageCollection { get; protected set; }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            DamageCollection = new List<(int, uint)>();
+        }
+
+        public override void Load(TagCompound tag)
+        {
+            base.Load(tag);
+
+            On.Terraria.Player.getDPS += DpsHijacker;
+        }
+
+        public override void OnHitAnythingWithDamage(int damage, float knockback, bool crit)
+        {
+            base.OnHitAnythingWithDamage(damage, knockback, crit);
+
+            DamageCollection.Add((damage, Main.GameUpdateCount));
+        }
+
+        protected static int DpsHijacker(On.Terraria.Player.orig_getDPS orig, Player self)
+        {
+            DpsTrackerModifierPlayer modifierPlayer = self.GetModPlayer<DpsTrackerModifierPlayer>();
+
+            for (int i = 0; i < modifierPlayer.DamageCollection.Count; i++)
+                if (modifierPlayer.DamageCollection[i].Item2 < Main.GameUpdateCount - 60)
+                    modifierPlayer.DamageCollection.RemoveAt(i);
+
+            return modifierPlayer.DamageCollection.Sum(x => x.Item1);
+        }
+    }
+}
--- Content/Players/RejuvenaPlayer.cs
+++ Content/Players/RejuvenaPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ModLoader;
 
 namespace Rejuvena.Content.Players
 {
@@ -7,5 +8,37 @@ namespace Rejuvena.Content.Players
     /// </summary>
     public abstract class RejuvenaPlayer : ModPlayer
     {
+        public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
+        {
+            base.OnHitNPC(item, target, damage, knockback, crit);
+
+            OnHitAnythingWithDamage(damage, knockback, crit);
+        }
+
+        public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
+        {
+            base.OnHitNPCWithProj(proj, target, damage, knockback, crit);
+
+            OnHitAnythingWithDamage(damage, knockback, crit);
+
+        }
+
+        public override void OnHitPvp(Item item, Player target, int damage, bool crit)
+        {
+            base.OnHitPvp(item, target, damage, crit);
+
+            OnHitAnythingWithDamage(damage, 0f, crit);
+        }
+
+        public override void OnHitPvpWithProj(Projectile proj, Player target, int damage, bool crit)
+        {
+            base.OnHitPvpWithProj(proj, target, damage, crit);
+
+            OnHitAnythingWithDamage(damage, 0f, crit);
+        }
+
+        public virtual void OnHitAnythingWithDamage(int damage, float knockback, bool crit)
+        {
+        }
     }
 }
