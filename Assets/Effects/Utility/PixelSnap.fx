af06314585c6d089075dbb2635de0914dcc37de1
Stevie <xxlennygamerxx@gmail.com>
2021-08-29 19:02:24 -0700

    Draw behind some bitches.

---
+++ Assets/Effects/Utility/PixelSnap.fx
@@ -0,0 +1,30 @@
+﻿sampler uImage0 : register(s0);
+sampler uImage1 : register(s1);
+float3 uColor;
+float3 uSecondaryColor;
+float uOpacity;
+float uSaturation;
+float uRotation;
+float uTime;
+float4 uSourceRect;
+float2 uWorldPosition;
+float2 uTargetPosition;
+float uDirection;
+float3 uLightSource;
+float2 uImageSize0;
+float2 uImageSize1;
+float4 uLegacyArmorSourceRect;
+float2 uLegacyArmorSheetSize;
+
+float4 PixelSnap(float4 sampleColor : COLOR0, float2 coords : TEXCOORD0) : COLOR0
+{
+    return sampleColor;
+}
+
+technique Technique1
+{
+    pass WavyPass
+    {
+        PixelShader = compile ps_2_0 PixelSnap();
+    }
+}
--- Content/Projectiles/RejuvenaProjectile.cs
+++ Content/Projectiles/RejuvenaProjectile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Rejuvena.Assets;
 using Terraria;
 using Terraria.ModLoader;
@@ -10,6 +11,16 @@ namespace Rejuvena.Content.Projectiles
     /// </summary>
     public abstract class RejuvenaProjectile : ModProjectile
     {
+        public enum DrawBehindType
+        {
+            Default,
+            NPCsAndTiles,
+            NPCs,
+            Projectiles,
+            OverPlayers,
+            OverWiresUI
+        }
+
         [Flags]
         public enum Defaults
         {
@@ -18,6 +29,8 @@ namespace Rejuvena.Content.Projectiles
         }
 
         public override string Texture => FallbackAsset.GetFallbackAsset(GetType(), base.Texture).Path;
+
+        public virtual DrawBehindType BehindType { get; set; } = DrawBehindType.Default;
 
         public void SetDefaultsFromEnum(Defaults defaultsToSet)
         {
@@ -31,5 +44,56 @@ namespace Rejuvena.Content.Projectiles
         }
 
         public Player GetOwner() => Main.player[Projectile.owner];
+
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs,
+            List<int> behindProjectiles, List<int> overPlayers,
+            List<int> overWiresUI)
+        {
+            static void SafeRemove(int index, ICollection<int> list)
+            {
+                if (list.Contains(index))
+                    list.Remove(index);
+            }
+
+            void ClearIndex()
+            {
+                SafeRemove(index, behindNPCsAndTiles);
+                SafeRemove(index, behindNPCs);
+                SafeRemove(index, behindProjectiles);
+                SafeRemove(index, overPlayers);
+                SafeRemove(index, overWiresUI);
+            }
+
+            switch (BehindType)
+            {
+                case DrawBehindType.Default:
+                    ClearIndex();
+                    break;
+
+                case DrawBehindType.NPCsAndTiles:
+                    behindNPCsAndTiles.Add(index);
+                    break;
+
+                case DrawBehindType.NPCs:
+                    behindNPCs.Add(index);
+                    break;
+
+                case DrawBehindType.Projectiles:
+                    behindProjectiles.Add(index);
+                    break;
+
+                case DrawBehindType.OverPlayers:
+                    overPlayers.Add(index);
+                    break;
+
+                case DrawBehindType.OverWiresUI:
+                    overWiresUI.Add(index);
+                    break;
+
+                default:
+                    ClearIndex();
+                    break;
+            }
+        }
     }
 }
