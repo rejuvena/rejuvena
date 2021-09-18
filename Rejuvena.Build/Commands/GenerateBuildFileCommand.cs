5e6412711827e3f8ff50e683dfb07949756ab436
Stevie <xxlennygamerxx@gmail.com>
2021-09-18 15:21:40 -0700

    Automatic build.txt file generation.

---
+++ Rejuvena.Build/Commands/GenerateBuildFileCommand.cs
@@ -0,0 +1,60 @@
+﻿#region License
+// Copyright (C) 2021 Tomat and Contributors
+// GNU General Public License Version 3, 29 June 2007
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using Rejuvena.BuildConfigurer;
+using Spectre.Console;
+
+namespace Rejuvena.Build.Commands
+{
+    [Command]
+    public class GenerateBuildFileCommand : ICommand
+    {
+        public class RejuvenaBuild : BuildFile
+        {
+            public RejuvenaBuild()
+            {
+                DisplayName = "Rejuvena";
+                Author = "Tomat and ENNWAY";
+                ModReferences = SortAfter = new List<string> {"TomatoLib"};
+                BuildIgnore = new List<string>
+                {
+                    ".git/*",
+                    ".vs/*",
+                    "bin/*",
+                    "obj/*",
+                    "TomatoLib/*",
+                    "Rejuvena.Tests/*",
+                    "Rejuvena.Build/*",
+                    "workshop.json"
+                };
+            }
+        }
+
+        [CommandOption("version", 'v', Description = "Assembly version.", IsRequired = true)]
+        public string Version { get; init; } = "0.0.0.1";
+
+        [CommandOption("output-dir", 'o', Description = "File output directory.", IsRequired = true)]
+        public string OutputDir { get; init; }
+
+        public virtual async ValueTask ExecuteAsync(IConsole console)
+        {
+            RejuvenaBuild build = new()
+            {
+                Version = System.Version.Parse(Version)
+            };
+
+            AnsiConsole.WriteLine($"Writing to file: {OutputDir}");
+
+            await File.WriteAllTextAsync(OutputDir, build.WriteFile());
+        }
+    }
+}
---
+++ Rejuvena.Build/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+
+namespace Rejuvena.Build
+{
+    public static class Program
+    {
+        public static async Task<int> Main() =>
+            await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .Build()
+                .RunAsync();
+    }
+}
