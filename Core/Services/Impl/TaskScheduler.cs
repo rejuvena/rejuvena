2bbc78669716f5431b095d470ffe4c7007f23653
Steviegt6 <xxlennygamerxx@gmail.com>
2021-11-07 20:11:56 -0800

    Scheduled task service.

---
+++ Core/Services/Impl/TaskScheduler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace Rejuvena.Core.Services.Impl
+{
+    public class TaskScheduler : Service
+    {
+        public List<Func<bool>> Tasks = new();
+
+        public override void Load()
+        {
+            base.Load();
+
+            On.Terraria.Main.Update += ExecutePostTasks;
+        }
+
+        public override void Unload()
+        {
+            base.Unload();
+
+            Tasks.Clear();
+        }
+
+        private void ExecutePostTasks(On.Terraria.Main.orig_Update orig, Main self, GameTime gameTime)
+        {
+            orig(self, gameTime);
+
+            Tasks.RemoveAll(x => x.Invoke());
+        }
+    }
+}
--- Rejuvena.cs
+++ Rejuvena.cs
@@ -1,11 +1,31 @@
 using System.Linq;
 using Rejuvena.Core.Services;
+using Rejuvena.Core.Services.Impl;
+using Terraria;
+using Terraria.GameContent.UI.States;
+using Terraria.ID;
 using TomatoLib;
 
 namespace Rejuvena
 {
     public class Rejuvena : TomatoMod
     {
-        public TService GetService<TService>() where TService : Service => GetContent<TService>().FirstOrDefault();
+        public override void PostSetupContent()
+        {
+            base.PostSetupContent();
+
+            GetService<TaskScheduler>().Tasks.Add(() =>
+            {
+                if (Main.menuMode != MenuID.Title) 
+                    return false;
+
+                Main.menuMode = MenuID.FancyUI;
+                Main.MenuUI.SetState(new UIAchievementsMenu());
+
+                return true;
+            });
+        }
+
+        public TService GetService<TService>() where TService : Service => GetContent<TService>().First();
     }
 }
