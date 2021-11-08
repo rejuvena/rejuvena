d85fe79219014d5bd87dbde1e1b38669a87dfdb3
Steviegt6 <xxlennygamerxx@gmail.com>
2021-11-07 22:06:17 -0800

    Begin to port over my tML PR (https://github.com/tModLoader/tModLoader/pull/1358) for MenuButtons.

---
+++ Core/Services/MenuModes/MenuButton.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+// Copied from my old tML PR: https://github.com/tModLoader/tModLoader/pull/1358
+
+namespace Rejuvena.Core.Services.MenuModes
+{
+    /// <summary>
+    ///     This class serves as a way to store information about menu buttons on the main menu. <br />
+    /// </summary>
+    public class MenuButton
+    {
+        /// <summary>
+        /// The name of the mod that added this menu button. For vanilla buttons, the mod name will be "Terraria".
+        /// </summary>
+        public string Mod { get; }
+
+        /// <summary>
+        /// The name of this menu button, which is used to help identify its function.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Allows you to tell the button what to do when it is left-clicked.
+        /// </summary>
+        public Action? OnLeftClick;
+
+        /// <summary>
+        /// Allows you to tell the button what to do when it is right-clicked;
+        /// </summary>
+        public Action? OnRightClick;
+
+        /// <summary>
+        /// Allows you to tell the button what to do when it's being hovered on.
+        /// </summary>
+        public Action? OnHover;
+
+        /// <summary>
+        /// The text this menu button displays.
+        /// </summary>
+        public string Text;
+
+        /// <summary>
+        /// The button's scale.
+        /// </summary>
+        public float Scale = 1f;
+
+        /// <summary>
+        /// Offset position on the y-axis.
+        /// </summary>
+        public int YOffsetPos = 0;
+
+        /// <summary>
+        /// Offset position on the x-axis.
+        /// </summary>
+        public int XOffsetPos = 0;
+
+        /// <summary>
+        /// If set to true, this text will be colored to white and the player will be unable to click on or hover over the button.
+        /// </summary>
+        public bool ReadonlyText = true;
+
+        /// <summary>
+        /// Similar to <see cref="readonlyText"/>, but just disallows clicking and hovering while still keeping the menu button color the same.
+        /// </summary>
+        public bool UnhoverableText = true;
+
+        /// <summary>
+        /// The color of the button text.
+        /// </summary>
+        public Color Color = Color.White;
+
+        // Internal fields used for Vanilla code.
+        internal bool LoweredAlpha = false;
+        internal bool NoCenterOffset = false;
+        internal byte ColorByte = 0;
+
+        public MenuButton(Mod mod, string name, string text)
+        {
+            Mod = mod.Name;
+            Name = name;
+            Text = text;
+        }
+
+        internal MenuButton(string name, string text)
+        {
+            Mod = "Terraria";
+            Name = name;
+            Text = text;
+        }
+	}
+}
--- Core/Services/MenuModes/MenuModeHandler.cs
+++ Core/Services/MenuModes/MenuModeHandler.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Localization;
+using TomatoLib.Common.Utilities.Extensions;
 
 namespace Rejuvena.Core.Services.MenuModes
 {
@@ -62,5 +65,108 @@ namespace Rejuvena.Core.Services.MenuModes
                 ref backButtonDown
             );
         }
+
+        public static List<MenuButton> ButtonList(
+            ref int numButtons,
+            ref bool[] readonlyText,
+            ref bool[] unhoverableText,
+            ref bool[] loweredAlpha,
+            ref int[] yOffsetPos,
+            ref int[] xOffsetPos,
+            ref byte[] color,
+            ref float[] scale,
+            ref bool[] noCenterOffset,
+            ref string[] text,
+            Color defaultColor,
+            out Color[] buttonColor,
+            out Action?[] onLeftClick,
+            out Action?[] onRightClick,
+            out Action?[] onHover
+        )
+        {
+            List<MenuButton> buttons = new();
+
+            for (int i = 0; i < numButtons; i++)
+            {
+                string buttonText = text[i]; // Can't use ref params in lambdas.
+
+                // Find the respective localization key for a button.
+                // Fallback to <MenuMod>_<Index>Button for non-localized buttons.
+                string buttonName = LanguageManager.Instance.GetFieldValue<
+                    LanguageManager, Dictionary<string, LocalizedText
+                    >>("_localizedTexts").Values.FirstOrDefault(
+                    x => x.Value == buttonText
+                )?.Key ?? $"{Main.menuMode}_{i}Button";
+
+                // Create a Vanilla menu button instance.
+                MenuButton button = new(buttonName, text[i]);
+
+                // Copy ALL data over to the newly-created MenuButton instance.
+                button.Color = GetColorFromByte(button.ColorByte = color[i], defaultColor); // Find the color based on the byte value Vanilla uses.
+                button.LoweredAlpha = loweredAlpha[i];
+                button.NoCenterOffset = noCenterOffset[i];
+                button.ReadonlyText = readonlyText[i];
+                button.Scale = scale[i];
+                button.Text = text[i];
+                button.UnhoverableText = unhoverableText[i];
+                button.XOffsetPos = xOffsetPos[i];
+                button.YOffsetPos = yOffsetPos[i];
+                buttons.Add(button);
+            }
+
+            // TODO: Modification method goes here.
+
+            // Resize and repopulate arrays with new data.
+            numButtons = buttons.Count;
+            readonlyText = new bool[numButtons];
+            unhoverableText = new bool[numButtons];
+            loweredAlpha = new bool[numButtons];
+            yOffsetPos = new int[numButtons];
+            xOffsetPos = new int[numButtons];
+            color = new byte[numButtons];
+            buttonColor = new Color[numButtons];
+            scale = new float[numButtons];
+            noCenterOffset = new bool[numButtons];
+            text = new string[numButtons];
+            onLeftClick = new Action[numButtons];
+            onRightClick = new Action[numButtons];
+            onHover = new Action[numButtons];
+
+
+            for (int i = 0; i < numButtons; i++)
+            {
+                readonlyText[i] = buttons[i].ReadonlyText;
+                unhoverableText[i] = buttons[i].UnhoverableText;
+                loweredAlpha[i] = buttons[i].LoweredAlpha;
+                yOffsetPos[i] = buttons[i].YOffsetPos;
+                xOffsetPos[i] = buttons[i].XOffsetPos;
+                color[i] = buttons[i].ColorByte;
+                buttonColor[i] = buttons[i].Color;
+                scale[i] = buttons[i].Scale;
+                noCenterOffset[i] = buttons[i].NoCenterOffset;
+                text[i] = buttons[i].Text;
+                onLeftClick[i] = buttons[i].OnLeftClick;
+                onRightClick[i] = buttons[i].OnRightClick;
+                onHover[i] = buttons[i].OnHover;
+            }
+
+            return buttons;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="Color"/> identical whatever color vanilla would use. <br />
+        /// This replaces the code vanilla uses for getting <see cref="MenuButton"/> colors.
+        /// </summary>
+        private static Color GetColorFromByte(byte color, Color defaultColor) =>
+            color switch
+            {
+                1 => Main.mcColor,
+                2 => Main.hcColor,
+                3 => Main.highVersionColor,
+                4 => Main.errorColor,
+                5 => Main.errorColor,
+                6 => Main.errorColor,
+                _ => defaultColor
+            };
     }
 }
