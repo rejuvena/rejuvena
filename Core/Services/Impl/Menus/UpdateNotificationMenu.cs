f455bdd2bd18d7cc2beb7ce355582d9b8dd3d80f
Tomat <xxlennygamerxx@gmail.com>
2021-11-08 11:56:07 -0800

    Improve update menu.

--- Core/Services/Impl/Menus/UpdateNotificationMenu.cs
+++ Core/Services/Impl/Menus/UpdateNotificationMenu.cs
@@ -3,20 +3,33 @@ using Microsoft.Xna.Framework;
 using Rejuvena.Core.Services.MenuModes;
 using Terraria.ModLoader;
 
+// TODO: Localization.
 namespace Rejuvena.Core.Services.Impl.Menus
 {
     public class UpdateNotificationMenu : Menu
     {
         public override void ModifyMenu(List<MenuButton> buttons)
         {
+            // Clear filler button provided for an IL hack.
             buttons.Clear();
+            
+            Mod mod = ModContent.GetInstance<Rejuvena>();
 
-            MenuButton button = new(ModContent.GetInstance<Rejuvena>(), "TestButton", "Test")
+            MenuButton button = new(mod, "UpdateNotif", "Update available!")
             {
                 Color = Color.Goldenrod
             };
 
-            buttons.Add(button);
+            buttons.Add(new MenuButton(mod, "UpdateAvailable", "An update for Rejuvena is available."));
+            
+            // TODO: Continue button after we finish some other IL stuff.
+            buttons.Add(new MenuButton(mod, "ContinueButton", "Continue")
+            {
+                Scale = 0.55f,
+                XOffsetPos = 80,
+                ReadonlyText = false,
+                UnhoverableText = false
+            });
         }
     }
 }
